26b1a2b356db471dd59c3a7324b6c6279ed8e3b2
Erik de Lima Vicente <erikdelimavicente@gmail.com>
2021-07-12 17:27:52 -0300

    commit

--- routes-api/src/routes/entities/route.entity.ts
+++ routes-api/src/routes/entities/route.entity.ts
@@ -11,28 +11,13 @@ export class Route {
 
 export const routes: Route[] = [
   {
-    title: 'De Manaus até Palmas',
-    startPosition: { lat: -3.1317917, long: -60.0204352 },
-    endPosition: { lat: -10.2429431, long: -48.347958 },
+    title: 'De Osasco até Fortaleza',
+    startPosition: { lat: -23.5317, long: -46.7899 },
+    endPosition: { lat: -3.71839, long: -38.5434 },
   },
   {
-    title: 'De Belo horizonte até Rio de Janeiro',
-    startPosition: { lat: -19.9161684, long: -43.9454672 },
+    title: 'De São Luis até Rio de Janeiro',
+    startPosition: { lat: -2.53073, long: -44.3068 },
     endPosition: { lat: -22.9081269, long: -43.1848695 },
-  },
-  {
-    title: 'De Londrina até Curitiba',
-    startPosition: { lat: -23.3453696, long: -51.0966428 },
-    endPosition: { lat: -25.4953187, long: -49.2761781 },
-  },
-  {
-    title: 'De São Luiz até Belém',
-    startPosition: { lat: -2.6351682, long: -44.2986359 },
-    endPosition: { lat: -1.4486251, long: -48.4728933 },
-  },
-  {
-    title: 'De Florianópolis até Porto Alegre',
-    startPosition: { lat: -27.6055634, long: -48.5757742 },
-    endPosition: { lat: -30.0212198, long: -51.1604492 },
   },
 ];
